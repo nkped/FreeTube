4f5df123085c5afa7f5313e06d34a933b0da16c1
nkped <nikrpedev@gmail.com>
2025-04-15 15:46:38 +0200

    add movie controller, view

---
+++ FreeTube/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using FreeTube.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FreeTube.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly FreeTubeContext _db;
+        public MoviesController(FreeTubeContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View(_db.Movies.Include("Genre").ToList());
+        }
+    }
+}

d8180cf543eda058c32e5930be07997231d4ecbc
nkped <nikrpedev@gmail.com>
2025-04-28 11:57:24 +0200

    add some updates

--- FreeTube/ViewModels/NewCustomerViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using FreeTube.Models;
-namespace FreeTube.ViewModels
-{
-    public class NewCustomerViewModel
-    {
-        public IEnumerable<MembershipType> MembershipTypes { get; set; }
-        public Customer Customer { get; set; }
-    }
-}

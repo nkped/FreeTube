1594f4085b48fefec47e78d3bbd5517d6f730552
nkped <nikrpedev@gmail.com>
2025-04-30 11:32:27 +0200

    fix !Model.Isvalid by making Membership prop in Customer model nullable (?)

--- FreeTube/Models/Customer.cs
+++ FreeTube/Models/Customer.cs
@@ -6,18 +6,20 @@ namespace FreeTube.Models
     {
         public int Id { get; set; }
         
-        [Required]
+        [Required(ErrorMessage = "Please select Name")]
         [StringLength(255)]
-        public string Name { get; set; }
-        
+        public string? Name { get; set; }
+
+        [Min18YearsIfMember]
         [Display(Name = "Date of Birth")]
         public DateTime? Birthdate { get; set; }
         
         public bool IsSubscribedToNewsletter { get; set; }
-        
+
+        [Required(ErrorMessage = "Please select Membership Type")]
         [Display(Name = "Membership Type")]
         public byte MembershipTypeId { get; set; }
         
-        public MembershipType MembershipType { get; set; }
+        public MembershipType? MembershipType { get; set; }
     }
 }

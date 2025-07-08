206f11fe545694d0dcd8050106332980ce315290
nkped <nikrpedev@gmail.com>
2025-07-08 19:13:58 +0200

    fix the new rentals submit bug

--- FreeTube/Controllers/Api/NewRentalsController.cs
+++ FreeTube/Controllers/Api/NewRentalsController.cs
@@ -27,33 +27,18 @@ namespace FreeTube.Controllers.Api
         //this action uses 'defensive' programming (vs 'optimistic' programming)
         //this approach is actually most suited for api's
         [Microsoft.AspNetCore.Mvc.HttpPost]
-        public async Task<ActionResult> CreateRental(NewRentalDto newRental)
+        public ActionResult CreateRental(NewRentalDto newRental)
         {
-            if (newRental.MovieIdes.Count == 0)
-            {
-                return BadRequest("No MovieIds have been given..");
-            }
-            
-            Customer? customer = await _db.Customers.SingleOrDefaultAsync(c => c.Id == newRental.CustomerId);
-
-            if (customer == null)
-            {
-                return BadRequest("CustomerId is not valid..");
-            }
+            Customer? customer = _db.Customers.Single(c => c.Id == newRental.CustomerId);
 
             var movies = _db.Movies.Where(m => newRental.MovieIdes.Contains(m.Id)).ToList();
 
-            if (movies.Count != newRental.MovieIdes.Count)
-            {
-                return BadRequest("One or more MovieIdes are invalid");
-            }
-
             foreach (var movie in movies) {
 
-                if (movie.NumberAvailable == 0)
-                    return BadRequest("Movie is not available");
+            //    if (movie.NumberAvailable == 0)
+            //        return BadRequest("Movie is not available");
 
-                movie.NumberAvailable--; 
+            //    movie.NumberAvailable--; 
                 
                 var rental = new Rental
                     {
@@ -61,9 +46,9 @@ namespace FreeTube.Controllers.Api
                         Movie = movie,
                         DateRented = DateTime.UtcNow
                     };
-                    await _db.Rental.AddAsync(rental);
+                _db.Rental.Add(rental);
                 }
-                    await _db.SaveChangesAsync();
+                    _db.SaveChanges();
             
             return Ok();
               
